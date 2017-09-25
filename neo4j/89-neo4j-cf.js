717ce2c6467d676ae99c5bbab877aa284a0b8fd0
gaskaj <joegaska@gmail.com>
2017-09-25 08:15:50 -0400

    Modified connectivity logic

--- neo4j/89-neo4j-cf.js
+++ neo4j/89-neo4j-cf.js
@@ -4,23 +4,36 @@ module.exports = function(RED) {
     function Neo4jNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
-        
-        console.log(config.url);
-        var db = new neo4j(config.url);
+        var neourl = config.url;
         var ctx = this;
+
         this.on('input', function(msg) {
-        	var query = config.query;
-            // Override static query with dynamic 
-            if (msg.query){
-                query=msg.query;
-                console.log("Overriding Cypher Query : " + query);
+
+            // If Config URL is empty and passed in within the msg use it.
+            // This allows dynamic connectivity.
+            if (!neourl && msg.url){
+                neourl = msg.url;
             }
-            var params = msg.payload;
-            db.cypherQuery(query, params, function (err, results){
-        		if (err) ctx.error(err);
-                msg.payload = results;
-        		node.send(msg);
-        	});           
+
+            if (neourl){
+                console.log(neourl);
+                var query = config.query;
+                // Override static query with dynamic
+                if (msg.query){
+                    query=msg.query;
+                    console.log("Cypher Query : " + query);
+                }
+                var params = msg.payload;
+                var db = new neo4j(neourl);
+                db.cypherQuery(query, params, function (err, results){
+                    if (err) ctx.error(err);
+                    msg.payload = results;
+                    node.send(msg);
+                });
+            }else{
+                node.error("Neo4j URL is Empty");
+            }
+
         });
     }
     RED.nodes.registerType("neo4j",Neo4jNode);
