dfadad69ae5eb3783ebccf6d172dcde6f6a217dc
Oscar Rovira <oscarrovira@me.com>
2018-01-22 16:55:23 +0100

    Update module to use official neo4j's node driver.

--- neo4j/89-neo4j-cf.js
+++ neo4j/89-neo4j-cf.js
@@ -1,47 +1,59 @@
-var neo4j = require("node-neo4j")
+var neo4j = require("neo4j-driver").v1
 
 module.exports = function(RED) {
-    function Neo4jNode(config) {
-        RED.nodes.createNode(this,config);
-        var node = this;
-        var neourl = config.url;
-        var ctx = this;
+  function Neo4jNode(config) {
+    RED.nodes.createNode(this,config);
+    var node = this;
+    var neourl = config.url;
+    var neouser = config.user;
+    var neopassword = config.password;
+    var ctx = this;
+    var driver = neo4j.driver(neourl, neo4j.auth.basic(neouser, neopassword));
 
-        this.on('input', function(msg) {
+    this.on('input', function(msg) {
+      // If Config URL is empty and passed in within the msg use it.
+      // This allows dynamic connectivity.
+      if (msg.url){
+        neourl = msg.url;
+      }
 
-            // If Config URL is empty and passed in within the msg use it.
-            // This allows dynamic connectivity.
-            if (msg.url){
-                neourl = msg.url;
+      if (neourl){
+        console.log(neourl);
+        var query = config.query;
+        // Override static query with dynamic
+        if (msg.query){
+            query=msg.query;
+            // console.log("Cypher Query : " + query);
+        }
+        var session = driver.session();
+        var results = [];
+        var params = msg.payload;
+
+        // Run a Cypher statement, reading the result in a streaming manner as records arrive:
+        session
+          .run(query, params)
+          .subscribe({
+            onNext: function (record) {
+              results.push(record.toObject());
+            },
+            onCompleted: function () {
+              session.close();
+              msg.payload = results;
+              node.send(msg);
+            },
+            onError: function (error) {
+              console.log("[neo4j Error] " + error.toString());
             }
+          });
+      }else{
+        node.error("Neo4j URL is Empty");
+      }
 
-            if (neourl){
-                //console.log(neourl);
-                var query = config.query;
-                // Override static query with dynamic
-                if (msg.query){
-                    query=msg.query;
-                    // console.log("Cypher Query : " + query);
-                }
+    });
 
-                try{
-                    var params = msg.payload;
-                    var db = new neo4j(neourl);
-                    db.cypherQuery(query, params, function (err, results){
-                        if (err) ctx.error(err);
-                        msg.payload = results;
-                        node.send(msg);
-                    });
-                }
-                catch(exception){
-                    console.log("[neo4j Exception] " + exception.toString());
-                    node.error(exception.toString());
-                }
-            }else{
-                node.error("Neo4j URL is Empty");
-            }
-
-        });
-    }
-    RED.nodes.registerType("neo4j",Neo4jNode);
+    this.on('close', function() {
+      driver.close();
+    })
+  }
+  RED.nodes.registerType("neo4j",Neo4jNode);
 }
